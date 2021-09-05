b570e66692de4ba9178ff09fed3cdec59a4c88d0
Dominykas Grubys <dominykas@Dominykass-MacBook-Pro.local>
2021-09-05 18:45:36 +0300

    Add mobile disclaimer

---
+++ src/pages/MobileDisclaimerFullscreen.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="mobile-disclaimer">
+    <BaseSvg
+      class="mobile-disclaimer__logo"
+      name="klaryLogo"
+    />
+    <div class="mobile-disclaimer__message">
+      <BaseSvg
+        class="mobile-disclaimer__icon"
+        name="mobile-disclaimer"
+      />
+      <h4 class="h4 mb-16">
+        The mobile experience is on the way
+      </h4>
+      <p class="b1 mb-16">
+        We are working as fast as we can to adapt Kuri to smaller devices.
+        It takes time because we want to create an exceptionally good experience for you.
+      </p>
+      <p class="b1">
+        For now, please use the desktop version. 🙏
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+.mobile-disclaimer {
+  position: relative;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 100000;
+  display: none;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  background: white;
+
+  &__logo {
+    position: absolute;
+    top: 24px;
+    right: 0;
+    left: 0;
+    width: 106px;
+    height: 40px;
+    margin: auto;
+  }
+
+  &__icon {
+    width: 56px;
+    height: 93px;
+    margin-bottom: 24px;
+  }
+
+  &__message {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    max-width: 460px;
+    text-align: center;
+  }
+
+  @media screen and (max-width: 950px) {
+    display: flex;
+  }
+}
+
+</style>
